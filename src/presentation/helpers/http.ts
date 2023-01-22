b55ad20d32ab963b6078d541509f95149e1d1742
Romário Dev <romarioarruda98@gmail.com>
2023-01-22 15:45:31 -0300

    feat(helpers): add serverError method to handle internal errors

--- src/presentation/helpers/http.ts
+++ src/presentation/helpers/http.ts
@@ -1,7 +1,13 @@
 import { MissingParamError } from "../errors/missing-param-error";
+import { ServerError } from "../errors/server-error";
 import { HttpResponse } from "../protocols/http";
 
 export const badRequest = (error: MissingParamError): HttpResponse => ({
   statusCode: 400,
   body: error
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error)
 })
