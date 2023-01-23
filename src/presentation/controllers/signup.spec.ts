2334dad6cfdb2e626373a547c46fdb3f76f31e16
Romário Dev <romarioarruda98@gmail.com>
2023-01-23 20:27:35 -0300

    refact(domain-use-cases): change use case name to email-service

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,4 +1,4 @@
-import { EmailService } from "../../domain/use-cases/email-validator";
+import { EmailService } from "../../domain/use-cases/email-service";
 import { InvalidParamError, MissingParamError, ServerError } from "../errors";
 import { SignUpController } from "./signup"
 
