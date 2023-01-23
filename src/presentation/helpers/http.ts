83e9c691f24a43915275a509992e336aa42c663b
Romário Dev <romarioarruda98@gmail.com>
2023-01-23 20:38:26 -0300

    refact(helper-http): change called to directly facade insted specific file

--- src/presentation/helpers/http.ts
+++ src/presentation/helpers/http.ts
@@ -1,6 +1,5 @@
-import { MissingParamError } from "../errors/missing-param-error";
-import { ServerError } from "../errors/server-error";
-import { HttpResponse } from "../protocols/http";
+import { MissingParamError, ServerError } from "../errors";
+import { HttpResponse } from "../protocols";
 
 export const badRequest = (error: MissingParamError): HttpResponse => ({
   statusCode: 400,
